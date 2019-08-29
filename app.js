9445efa6055dea3a9e51442c7c512f9e64fbacb7
vadimc <Vadim.Chumachenko@skypro.ch>
2019-08-29 13:40:41 +0300

    add logger

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const Telegraf = require('telegraf')
 const fs = require('fs')
 const env = JSON.parse(fs.readFileSync('./.env', 'utf8'))
 const bot = new Telegraf(env.botToken)
+const logger = require('./logs')
 
 bot.start((ctx) => {
     console.log('Id пользователя:', ctx.from.id);
@@ -13,10 +14,12 @@ bot.hears('Hi', ctx => {
     return ctx.reply('Hey!')
 })
 
+logger.appLogger.info('Start')
+
 try {
     bot.telegram.sendMessage(env.id.my, 'I ready')
 } catch (error) {
-
+    logger.appLogger.info(error)
 }
 
 bot.startPolling()
---
+++ logs.js
@@ -0,0 +1,22 @@
+const winston = require('winston')
+
+module.exports.appLogger = winston.createLogger({
+    level: 'info',
+    format: winston.format.json(),
+    transports: [
+        new (winston.transports.File)({
+            filename: "logs/" + 'info.log'
+        })
+    ]
+})
+
+
+module.exports.appLogger = winston.createLogger({
+    level: 'error',
+    format: winston.format.json(),
+    transports: [
+        new (winston.transports.File)({
+            filename: "logs/" + 'error.log'
+        })
+    ]
+})
